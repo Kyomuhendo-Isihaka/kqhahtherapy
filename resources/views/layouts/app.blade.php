8a8f7b2d26296b4890cfa19e1268b9e8c89eee11
kyomuhendo-isihaka <kyomuhendoisihaka1@gmail.com>
2024-09-08 22:44:02 +0300

    hi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,72 +31,74 @@
             @yield('content')
         </div>
 
-        <div id="cartSidePenal" drawer-end="" class="fixed inset-y-0 flex flex-col w-full transition-transform duration-300 ease-in-out transform bg-white shadow dark:bg-zink-600 ltr:right-0 rtl:left-0 md:w-96 z-drawer show">
-            <div class="flex items-center justify-between p-4 border-b border-slate-200 dark:border-zink-500">
-                <div class="grow">
-                    <h5 class="mb-0 text-16">Shopping Cart <span class="inline-flex items-center justify-center w-5 h-5 ml-1 text-[11px] font-medium border rounded-full text-white bg-custom-500 border-custom-500">3</span></h5>
-                </div>
-                <div class="shrink-0">
-                    <button data-drawer-close="cartSidePenal" class="transition-all duration-150 ease-linear text-slate-500 hover:text-slate-800"><i data-lucide="x" class="size-4"></i></button>
-                </div>
-            </div>
 
-            <div>
-                <div class="h-[calc(150vh_-_370px)] p-4 overflow-y-auto product-list">
-                    <div class="flex flex-col gap-4">
-                        <div class="flex gap-2 product">
-                            <div class="flex items-center justify-center w-12 h-12 rounded-md bg-slate-100 shrink-0 dark:bg-zink-500">
-                                <img src="{{ asset('uploads/kqhahtherapy.jpeg') }}" alt="" class="h-8">
-                            </div>
-                            <div class="overflow-hidden grow">
-                                <div class="ltr:float-right rtl:float-left">
-                                    <button class="transition-all duration-150 ease-linear text-slate-500 dark:text-zink-200 hover:text-red-500 dark:hover:text-red-500"><i data-lucide="x" class="size-4"></i></button>
-                                </div>
-                                <a href="{{ route('product.details', 1) }}" class="transition-all duration-200 ease-linear hover:text-custom-500">
-                                    <h6 class="mb-1 text-15">Gucii Oils</h6>
-                                </a>
-                                <div class="flex items-center mb-3">
-                                    <h5 class="text-base product-price"> $<span>35.5</span></h5>
-
-                                </div>
-                                <div class="flex items-center justify-between gap-3">
-                                    <div class="inline-flex text-center input-step">
-                                        <button type="button" class="border w-9 h-9 leading-[15px] minus bg-white dark:bg-zink-700 dark:border-zink-500 ltr:rounded-l rtl:rounded-r transition-all duration-200 ease-linear border-slate-200 text-slate-500 dark:text-zink-200 hover:bg-custom-500 dark:hover:bg-custom-500 hover:text-custom-50 dark:hover:text-custom-50 hover:border-custom-500 dark:hover:border-custom-500 focus:bg-custom-500 dark:focus:bg-custom-500 focus:border-custom-500 dark:focus:border-custom-500 focus:text-custom-50 dark:focus:text-custom-50"><i data-lucide="minus" class="inline-block size-4"></i></button>
-                                        <input type="number" class="w-12 text-center h-9 border-y product-quantity dark:bg-zink-700 focus:shadow-none dark:border-zink-500" value="2" min="0" max="100" readonly="">
-                                        <button type="button" class="transition-all duration-200 ease-linear bg-white border dark:bg-zink-700 dark:border-zink-500 ltr:rounded-r rtl:rounded-l w-9 h-9 border-slate-200 plus text-slate-500 dark:text-zink-200 hover:bg-custom-500 dark:hover:bg-custom-500 hover:text-custom-50 dark:hover:text-custom-50 hover:border-custom-500 dark:hover:border-custom-500 focus:bg-custom-500 dark:focus:bg-custom-500 focus:border-custom-500 dark:focus:border-custom-500 focus:text-custom-50 dark:focus:text-custom-50"><i data-lucide="plus" class="inline-block size-4"></i></button>
-                                    </div>
-                                    <h6 class="product-line-price">310.64</h6>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-                <div class="p-4 border-t border-slate-200 dark:border-zink-500">
-
-                    <table class="w-full mb-3 ">
-                        <tbody class="table-total">
-                            <tr>
-                                <td class="py-2">Sub Total :</td>
-                                <td class="text-right cart-subtotal">$2,847.55</td>
-                            </tr>
-
-                            <tr class="font-semibold">
-                                <td class="py-2">Total : </td>
-                                <td class="text-right cart-total">$2,531.17</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <div class="flex items-center justify-between gap-3">
-                        <a href="{{ route('home') }}" class="btn btn-dark">Continue Shopping</a>
-                        <a href="{{ route('cart') }}" class="btn btn-primary">Proceed to Cart</a>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 
     @include('layouts.footer')
+
+    {{-- <div id="cartSidePenal" drawer-end="" class="fixed inset-y-0 flex flex-col w-full transition-transform duration-300 ease-in-out transform bg-white shadow dark:bg-zink-600 ltr:right-0 rtl:left-0 md:w-96 z-drawer show">
+        <div class="flex items-center justify-between p-4 border-b border-slate-200 dark:border-zink-500">
+            <div class="grow">
+                <h5 class="mb-0 text-16">Shopping Cart <span class="inline-flex items-center justify-center w-5 h-5 ml-1 text-[11px] font-medium border rounded-full text-white bg-custom-500 border-custom-500">3</span></h5>
+            </div>
+            <div class="shrink-0">
+                <button data-drawer-close="cartSidePenal" class="transition-all duration-150 ease-linear text-slate-500 hover:text-slate-800"><i data-lucide="x" class="size-4"></i></button>
+            </div>
+        </div>
+
+        <div>
+            <div class="h-[calc(150vh_-_370px)] p-4 overflow-y-auto product-list">
+                <div class="flex flex-col gap-4">
+                    <div class="flex gap-2 product">
+                        <div class="flex items-center justify-center w-12 h-12 rounded-md bg-slate-100 shrink-0 dark:bg-zink-500">
+                            <img src="{{ asset('uploads/kqhahtherapy.jpeg') }}" alt="" class="h-8">
+                        </div>
+                        <div class="overflow-hidden grow">
+                            <div class="ltr:float-right rtl:float-left">
+                                <button class="transition-all duration-150 ease-linear text-slate-500 dark:text-zink-200 hover:text-red-500 dark:hover:text-red-500"><i data-lucide="x" class="size-4"></i></button>
+                            </div>
+                            <a href="{{ route('product.details', 1) }}" class="transition-all duration-200 ease-linear hover:text-custom-500">
+                                <h6 class="mb-1 text-15">Gucii Oils</h6>
+                            </a>
+                            <div class="flex items-center mb-3">
+                                <h5 class="text-base product-price"> $<span>35.5</span></h5>
+
+                            </div>
+                            <div class="flex items-center justify-between gap-3">
+                                <div class="inline-flex text-center input-step">
+                                    <button type="button" class="border w-9 h-9 leading-[15px] minus bg-white dark:bg-zink-700 dark:border-zink-500 ltr:rounded-l rtl:rounded-r transition-all duration-200 ease-linear border-slate-200 text-slate-500 dark:text-zink-200 hover:bg-custom-500 dark:hover:bg-custom-500 hover:text-custom-50 dark:hover:text-custom-50 hover:border-custom-500 dark:hover:border-custom-500 focus:bg-custom-500 dark:focus:bg-custom-500 focus:border-custom-500 dark:focus:border-custom-500 focus:text-custom-50 dark:focus:text-custom-50"><i data-lucide="minus" class="inline-block size-4"></i></button>
+                                    <input type="number" class="w-12 text-center h-9 border-y product-quantity dark:bg-zink-700 focus:shadow-none dark:border-zink-500" value="2" min="0" max="100" readonly="">
+                                    <button type="button" class="transition-all duration-200 ease-linear bg-white border dark:bg-zink-700 dark:border-zink-500 ltr:rounded-r rtl:rounded-l w-9 h-9 border-slate-200 plus text-slate-500 dark:text-zink-200 hover:bg-custom-500 dark:hover:bg-custom-500 hover:text-custom-50 dark:hover:text-custom-50 hover:border-custom-500 dark:hover:border-custom-500 focus:bg-custom-500 dark:focus:bg-custom-500 focus:border-custom-500 dark:focus:border-custom-500 focus:text-custom-50 dark:focus:text-custom-50"><i data-lucide="plus" class="inline-block size-4"></i></button>
+                                </div>
+                                <h6 class="product-line-price">310.64</h6>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="p-4 border-t border-slate-200 dark:border-zink-500">
+
+                <table class="w-full mb-3 ">
+                    <tbody class="table-total">
+                        <tr>
+                            <td class="py-2">Sub Total :</td>
+                            <td class="text-right cart-subtotal">$2,847.55</td>
+                        </tr>
+
+                        <tr class="font-semibold">
+                            <td class="py-2">Total : </td>
+                            <td class="text-right cart-total">$2,531.17</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div class="flex items-center justify-between gap-3">
+                    <a href="{{ route('home') }}" class="btn btn-dark">Continue Shopping</a>
+                    <a href="{{ route('cart') }}" class="btn btn-primary">Proceed to Cart</a>
+                </div>
+            </div>
+        </div>
+    </div> --}}
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
