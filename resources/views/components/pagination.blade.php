9c611ef46a0d221af819ef31a397ec12ee8ff1c5
kyomuhendo-isihaka <kyomuhendoisihaka1@gmail.com>
2024-09-19 00:07:35 +0300

    pangation

--- resources/views/components/pagination.blade.php
+++ resources/views/components/pagination.blade.php
@@ -1,35 +1,43 @@
 @if ($paginator->lastPage() > 1)
-
 <nav>
-    <ul class="w3-pagination w3-margin-top">
+    <ul style="list-style-type: none; padding: 0; margin-top: 20px; display: flex;">
         @if ($paginator->onFirstPage())
-            <li class="w3-disabled"><span>&laquo; Previous</span></li>
+            <li style="margin-right: 5px; opacity: 0.5;"><span>&laquo; Previous</span></li>
         @else
-            <li><a href="{{ $paginator->previousPageUrl() }}" class="w3-button ">&laquo; Previous</a></li>
+            <li style="margin-right: 5px;">
+                <a href="{{ $paginator->previousPageUrl() }}" style="text-decoration: none; padding: 10px 15px; background-color: #f1f1f1; border: 1px solid #ccc; border-radius: 4px;">&laquo; Previous</a>
+            </li>
         @endif
 
         @foreach ($elements as $element)
             @if (is_string($element))
-                <li class="w3-disabled"><span>{{ $element }}</span></li>
+                <li style="margin-right: 5px; opacity: 0.5;"><span>{{ $element }}</span></li>
             @endif
 
             @if (is_array($element))
                 @foreach ($element as $page => $url)
                     @if ($page == $paginator->currentPage())
-                        <li class="bg-app"><span>{{ $page }}</span></li>
+                        <li style="margin-right: 5px; background-color: orange; color: white; padding: 10px 15px; border-radius: 4px;">
+                            <span>{{ $page }}</span>
+                        </li>
                     @else
-                        <li><a href="{{ $url }}" class="w3-button ">{{ $page }}</a></li>
+                        <li style="margin-right: 5px;">
+                            <a href="{{ $url }}" style="text-decoration: none; padding: 10px 15px; background-color: #f1f1f1; border: 1px solid #ccc; border-radius: 4px;">{{ $page }}</a>
+                        </li>
                     @endif
                 @endforeach
             @endif
         @endforeach
 
         @if ($paginator->hasMorePages())
-            <li><a href="{{ $paginator->nextPageUrl() }}" class="w3-button">Next &raquo;</a></li>
+            <li style="margin-right: 5px;">
+                <a href="{{ $paginator->nextPageUrl() }}" style="text-decoration: none; padding: 10px 15px; background-color: #f1f1f1; border: 1px solid #ccc; border-radius: 4px;">Next &raquo;</a>
+            </li>
         @else
-            <li class="w3-disabled"><span>Next &raquo;</span></li>
+            <li style="margin-right: 5px; opacity: 0.5;"><span>Next &raquo;</span></li>
         @endif
     </ul>
 </nav>
+
 @endif
 
